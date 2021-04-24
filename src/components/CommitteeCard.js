831d723df519d9a5982880e59011498139564575
Ben Silverman <bensilverman10@gmail.com>
2021-04-24 17:46:53 +0100

    Initial attempts at loading dynamic images

--- src/components/CommitteeCard.js
+++ src/components/CommitteeCard.js
@@ -1,30 +1,57 @@
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
 import styled from "styled-components";
 import PropTypes from "prop-types";
+
+import config from "../theme/config";
+const { color, font } = config;
 
 const Card = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  padding: 20px;
-  margin: 0 0.5em;
-  margin-bottom: 1em;
+  margin: 1em 0;
 `;
 
 const Photo = styled.img`
   margin-bottom: 0.5em;
 `;
 
+const Name = styled.h3`
+  font-size: ${font.size.lg};
+  margin-bottom: 0em;
+`;
+
+const Role = styled.h4`
+  color: ${color.accent};
+  font-weight: ${font.weight.semibold};
+  text-transform: uppercase;
+  font-size: ${font.size.base};
+  margin-bottom: 1em;
+`;
+
 import blankTeamPhoto from "../images/BlankTeamPhoto.jpg";
 
 const CommitteeCard = ({ role, fullName, pronouns, course, picture = false }) => {
+  const data = useStaticQuery(graphql`
+    {
+      avatar: file(sourceInstanceName: { eq: "images" }, relativePath: { eq: "BenTeamPhoto.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(width: 200, placeholder: TRACED_SVG, formats: [AUTO, WEBP, AVIF])
+        }
+      }
+    }
+  `);
+
   return (
     <Card>
       {picture ? picture : <Photo src={blankTeamPhoto} width="200px" height="200px" />}
-      <h2>{fullName}</h2>
-      <h3>{role}</h3>
+      <GatsbyImage image={data.avatar.childImageSharp.gatsbyImageData} alt="Profile image" />
+      <Name>{fullName}</Name>
+      <Role>{role}</Role>
       <p>{course}</p>
-      <p>{pronouns}</p>
+      {pronouns && <p>{pronouns}</p>}
     </Card>
   );
 };
